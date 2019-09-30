55c04b50fc467159db931a0ddd93c60b022e3c17
i.romanov <i.romanov@xsolla.com>
2019-09-30 11:32:00 +0500

    settings comment

--- QueueStore/app/settings.php
+++ QueueStore/app/settings.php
@@ -9,14 +9,14 @@ return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         'settings' => [
             'displayErrorDetails' => true, // Should be set to false in production
-            'workFrom' => 8, //
-            'workTill' => 20,
-            'idle' => 300,
-            'flowRate' => 0.1,
-            'timeForGoods'=> 60,
-            'timeForMoney'=>10,
-            'criticalLength' => 5,
-            'cashboxCount' => 4,
+            'workFrom' => 8, // начало рабочего дня
+            'workTill' => 20, // конец рабочего дня
+            'idle' => 300, // время простоя кассы после которого она закрывается
+            'flowRate' => 0.1, // вероятность появления посетителя в каждую секунду
+            'timeForGoods'=> 10, // время на проведение одной покупки
+            'timeForMoney'=>60, // время на оплату
+            'criticalLength' => 5, // длина очереди при которой открывается новая касса
+            'cashboxCount' => 4, // количество касс в магазине
             'logger' => [
                 'name' => 'slim-app',
                 'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
